0353433d94d3164b7392820cfaa93183247b749c
sandeepbnvh <sandeep.bnvh@gmail.com>
2020-10-02 05:47:25 +0000

    First Commit

---
+++ webapp/Component.js
@@ -0,0 +1,41 @@
+sap.ui.define([
+	"sap/ui/core/UIComponent",
+	"sap/ui/Device",
+	"ns/Automata/model/models"
+], function (UIComponent, Device, models) {
+	"use strict";
+
+	return UIComponent.extend("ns.Automata.Component", {
+
+		metadata: {
+			manifest: "json"
+		},
+
+		/**
+		 * The component is initialized by UI5 automatically during the startup of the app and calls the init method once.
+		 * @public
+		 * @override
+		 */
+		init: function () {
+			// call the base component's init function
+			UIComponent.prototype.init.apply(this, arguments);
+
+			// enable routing
+			this.getRouter().initialize();
+
+			// set the device model
+            this.setModel(models.createDeviceModel(), "device");
+            this.renderRecastChatbot();
+        },
+        renderRecastChatbot: function() {
+            if (!document.getElementById("cai-webchat")) {
+                var s = document.createElement("script");
+                   s.setAttribute("id", "cai-webchat");
+                  s.setAttribute("src", "https://cdn.cai.tools.sap/webchat/webchat.js");
+                      document.body.appendChild(s);
+                }
+                s.setAttribute("channelId", "2d0245f0-8964-4ad0-9ba9-f1c930f0f6fb");
+                s.setAttribute("token", "9d0f703e4f01eb3b6b2921a18d17e0e6");
+        },
+	});
+});
